397b8c6f09f9edd7190038f99d4624f06c65aec5
zacbailey <zbail25@wgu.ed>
2025-12-06 13:00:20 -0800

    Task H: Removed DeletePartValidator.java and ValidDeletePart.java

--- README.md
+++ README.md
@@ -61,4 +61,4 @@ I.  Add at least two unit tests for the maximum and minimum fields to the PartTe
 
 J.  Remove the class files for any unused validators in order to clean your code. 
 
-    *
+    * Removed DelePartValidator and ValidDeletePart. There was no usage. 
--- src/main/java/com/example/demo/domain/Part.java
+++ src/main/java/com/example/demo/domain/Part.java
@@ -1,6 +1,5 @@
 package com.example.demo.domain;
 
-import com.example.demo.validators.ValidDeletePart;
 import com.example.demo.validators.ValidInventory;
 
 import javax.persistence.*;
@@ -16,7 +15,6 @@ import java.util.Set;
  *
  */
 @Entity
-@ValidDeletePart
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name="part_type",discriminatorType = DiscriminatorType.INTEGER)
 @Table(name="Parts")
